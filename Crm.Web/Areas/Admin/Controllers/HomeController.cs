5e6f381f29878c33eae49a5423e175217ecda6db
Murat Demirci <mdemirci@outlook.com>
2019-03-06 11:04:49 +0300

    ASP.NET Identity ile Üyelik Sistemi örneği (Role sistemi ile birlikte)

---
+++ Crm.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crm.Web.Areas.Admin.Controllers
+{
+    [RouteArea("Admin")]
+    [Authorize(Roles = "User")]
+    public class HomeController : Controller
+    {
+        
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
